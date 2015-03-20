c0a1e7b932750a0737b311782110b611e84bfc5c
Manuel <manuel@mim-2.local>
2015-03-20 17:29:09 +0100

    removed direct facebook access

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,5 @@
 var keystone = require('keystone'),
-	Types = keystone.Field.Types,
-
-	fb = require('../lib/facebookFactory');
+	Types = keystone.Field.Types;
 
 /**
  * Post Model
@@ -20,14 +18,11 @@ Post.add({
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
 	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 }
-	},
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
-
-	publishOnFacebook: { type: Types.Boolean }, //maybe immutable after post
-	facebookText: { type: Types.Html, wysiwyg: false },
-	facebookPostId: { type: String, noedit: true, nocreate: true, nodelete: true }
+		heroImage: { type: Types.CloudinaryImage },
+		brief: { type: Types.Textarea, height: 150 },
+		images: { type: Types.CloudinaryImages },
+		extended: { type: Types.Markdown, height: 400 }
+	}
 });
 
 Post.schema.virtual('content.full').get(function() {
@@ -38,13 +33,5 @@ Post.schema.methods.isPublished = function() {
 	return this.state == 'published';
 };
 
-Post.schema.post('save', function() {	
-	var facebook = fb(process.env.FACEBOOK_ACCESS_TOKEN);
-	if(this.isPublished() && this.publishOnFacebook && !this.facebookPostId && this.facebookText) {
-		var that = this;
-		facebook.createPost(this.facebookText, function (id) { that.facebookPostId = id; that.save(); });
-	}
-});
-
-Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
+Post.defaultColumns = 'title, state|20%, publishedDate|20%';
 Post.register();
